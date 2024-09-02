203f7060a590b4f273ff852ba542980b7047dc2a
SanjayRavichandran-Mscss <117557388+SanjayRavichandran-Mscss@users.noreply.github.com>
2024-09-02 12:33:29 +0530

    adding style

--- screenRecorder/src/App.jsx
+++ screenRecorder/src/App.jsx
@@ -1,4 +1,4 @@
-import  { useRef } from 'react';
+import { useRef } from 'react';
 
 const ScreenRecorder = () => {
   const videoElement = useRef(null);
@@ -25,11 +25,29 @@ const ScreenRecorder = () => {
   };
 
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Screen Capture Application using Screen Capture API</h1>
-      <button onClick={startCapture}>Start Capture</button>
-      <button onClick={stopCapture}>Stop Capture</button>
-      <video id="video" ref={videoElement} autoPlay style={{ border: '2px solid #8b8585', width: '98%', maxWidth: '860px' }}></video>
+    <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
+      <video
+        id="video"
+        ref={videoElement}
+        autoPlay
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          objectFit: 'cover',
+        }}
+      ></video>
+      <div style={{ position: 'absolute', bottom: '20px', right: '20px' }}>
+        <button onClick={startCapture} style={{ backgroundColor: 'orange', color: 'white', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+          Start Capture
+        </button>
+        <button onClick={stopCapture} style={{ backgroundColor: 'red', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+          Stop Capture
+        </button>
+      </div>
     </div>
   );
 };
